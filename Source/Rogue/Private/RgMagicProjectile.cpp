98bcf2792413cc70307f3006d7592cc334acb69c
Jerry <jerrytjohn@gmail.com>
2024-06-01 00:45:00 +0530

    Improved projectile behavior

--- Source/Rogue/Private/RgMagicProjectile.cpp
+++ Source/Rogue/Private/RgMagicProjectile.cpp
@@ -14,6 +14,7 @@ ARgMagicProjectile::ARgMagicProjectile()
 	PrimaryActorTick.bCanEverTick = true;
 
 	SphereComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+	SphereComponent->SetCollisionProfileName("Projectile");
 	RootComponent = SphereComponent;
 
 	ParticleEffect = CreateDefaultSubobject<UParticleSystemComponent>("ParticleEffect");
