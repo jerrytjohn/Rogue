b88b526c9665717afd3064cc66ea523a45195ec9
Jerry <jerrytjohn@gmail.com>
2024-06-04 17:57:21 +0530

    Finally got the behavior tree to compile. Was missing an external library

---
+++ Source/Rogue/Private/RgBTServiceCheckAttackRange.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RgBTServiceCheckAttackRange.h"
+
+void URgBTServiceCheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
