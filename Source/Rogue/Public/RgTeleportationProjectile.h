0901858a1a870ddcae7bbae2db5f7a2a17428738
Jerry <jerrytjohn@gmail.com>
2024-06-05 18:11:24 +0530

    Added in the template for the teleportation projectile

---
+++ Source/Rogue/Public/RgTeleportationProjectile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RgProjectileBase.h"
+#include "RgTeleportationProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROGUE_API ARgTeleportationProjectile : public ARgProjectileBase
+{
+	GENERATED_BODY()
+	
+};
